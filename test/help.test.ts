f6244fe7be12b2dc0114274cc83f00039c050017
Jonathan Chaffer <jonathan.chaffer@hightouch.io>
2026-08-10 17:47:37 -0400

    Pin help width to 80 for consistency in CI

--- test/help.test.ts
+++ test/help.test.ts
@@ -1,13 +1,19 @@
+import { Command } from 'commander';
 import { buildProgram } from '../src/program';
+
+function helpText(command: Command): string {
+  command.configureHelp({ helpWidth: 80 });
+  return command.helpInformation();
+}
 
 describe('help output', () => {
   it('root help lists all commands and global options', () => {
-    expect(buildProgram().helpInformation()).toMatchSnapshot();
+    expect(helpText(buildProgram())).toMatchSnapshot();
   });
 
   it.each(['init', 'generate', 'check'])('%s --help', (name) => {
     const sub = buildProgram().commands.find((c) => c.name() === name);
     expect(sub).toBeDefined();
-    expect(sub!.helpInformation()).toMatchSnapshot();
+    expect(helpText(sub!)).toMatchSnapshot();
   });
 });
