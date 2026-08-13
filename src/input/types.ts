c901400d6c3cecf47395bdd9cb26b4d317d4f9c9
Jonathan Chaffer <jonathan.chaffer@hightouch.io>
2026-08-13 11:06:23 -0400

    Remove source slug from inputs & domain schema - not shown in the UI so not really useful for lookup

--- src/input/types.ts
+++ src/input/types.ts
@@ -42,7 +42,6 @@ export const domainSchema = z.object({
       z.object({
         id: z.string(),
         name: z.string(),
-        slug: z.string().optional(),
       }),
     )
     .optional(),
